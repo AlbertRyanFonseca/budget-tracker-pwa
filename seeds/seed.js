d75df64b652365a8b67d759a75dbbf9c345b6185
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 03:16:10 -0700

    Added a constant for mongoose connection to see if we are connected with all the future files with .env

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,6 @@
 var mongoose = require("mongoose");
 var db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-});
-
 var transactionSeed = [
     {
         name: "Christmas",
