43a1483bccadffe9e1afc759f1a2b6b7d4c1ce4d
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 02:26:41 -0700

    Added the dbCheck function, this will open the pending transaction, access it in the object store, then get all the records form the store and then assin it to a variable. We will then run through all the fetch, and if successful, we'll open the pending db. Then we will access the pending object store and cleat all in the store

--- public/assets/db/db.js
+++ public/assets/db/db.js
@@ -15,3 +15,25 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
     store.add(record);
 }
+
+function dbCheck() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transation/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: { Accept: "application/json, text/plain, */*", "Content-Type": "application/json", },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readewrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                })
+        }
+    }
+}
